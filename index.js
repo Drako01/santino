9e4c51074a67dfe778a838427fb134e75412bca0
Alejandro Di Stefano <addistefano@live.com.ar>
2025-06-16 11:03:42 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+// let numeroA = 1;
+// console.log(typeof numeroA)
+// const NUMERO_B = 2;
+// console.log( NUMERO_B)
+
+// numeroA = 6;
+// // NUMERO_B =3;
+// console.log(typeof numeroA)
+
+
+// numeroA = "Hola";
+// console.log(typeof numeroA)
+
+// const arrayLetras = [
+//     'a', 
+//     'b', 
+//     'c',
+//     's'
+// ];
+
+// arrayLetras.push('m', 'l')
+// // arrayLetras.pop()
+// console.log(arrayLetras)
+// const cantidadDeValores = arrayLetras.length
+
+// console.log(cantidadDeValores)
+
+// ///  Variable inicializada | Condicion          | Incremento (a cada vuelta le suma)
+// for (let i = 0; i < arrayLetras.length; i++){
+//     console.log("En el Indice: " + i + " esta el valor " + arrayLetras[i]);
+// }
+
+// console.log(arrayLetras[2])
+
+// let contador = 0;
+
+// for(let i = 0; i <= 100; i++){
+//     contador = i;
+//     console.log("mensaje Nro: " + contador)
+// }
+
+// const frutas = ["banana", "manzana", "pera", "anana"];
+
+// for(let i = 0; i < frutas.length; i++){
+//     console.log("Frutas: " + frutas[i]);
+// }
+// console.log(frutas.length)
+
+// frutas.push("kiwi")
+// frutas.shift() // Elimina el 1er Elemento
+// // for of
+// for(const fruta of frutas){
+//     console.log("Frutas: " + fruta);
+// }
+// console.log(frutas.length)
+
+// let indiceAEliminar = 3;
+
+// frutas.splice(indiceAEliminar, 1)
+
+// for(const fruta of frutas){
+//     console.log("Frutas: " + fruta);
+// }
+// console.log(frutas.length)
+
+// indiceAEliminar = 1;
+
+// frutas.splice(indiceAEliminar, 2)
+
+// for(const fruta of frutas){
+//     console.log("Frutas: " + fruta);
+// }
+// console.log(frutas.length)
+
+
