7a57dee6bda1c066effb38e8245c330c976def07
Alejandro Di Stefano <addistefano@live.com.ar>
2025-06-30 11:56:20 -0300

    Clase 06

--- js/productos.js
+++ js/productos.js
@@ -1,57 +1,87 @@
-// Referencia al contenedor
-const contenedor = document.getElementById('contenedor-productos');
-
-// Cargar y renderizar el json
-// console.log(fetch('../productos.json'))
-fetch('../productos.json')
-    .then(response => {
-        if(!response.ok) throw new Error("Error al cargar el archivo JSON");
-        const data = response.json();
-        // console.log(data)
-        return data;
-    })
-    .then(productos => {
-        productos.forEach(producto => {
-            // console.log(producto)
-            const div = document.createElement('div');
-            div.classList.add('producto');
-
-            div.innerHTML =  ` 
-                <h2>${producto.nombre}</h2>
-                <p><strong>Precio: </strong>$${producto.precio}</p>
-                <p>${producto.descripcion}</p>
-                <button type="button" id="producto-${producto.id}">Comprar</button>
-            `;
-
-            contenedor.appendChild(div);
-            const boton = document.querySelector(`#producto-${producto.id}`);
-            boton.addEventListener('click', () => mostrarDetalles(producto))
-        });
-    })
-    .catch(err => {
-        contenedor.innerHTML = `<p style="color: red;">${err.message}</p>`;
-    })
-
-
-function mostrarDetalles(producto){
-    let detalles = document.getElementById('detalle-producto');
-
-    if(!detalles){
-        detalles = document.createElement('div');
-        detalles.id = 'detalle-producto';
-        detalles.style.border = '2px solid #000';
-        detalles.style.marginTop = '20px';
-        detalles.style.padding = '10px';
-        document.body.appendChild(detalles);
+class Producto {
+    constructor(id, nombre, image, precio, descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.image = image;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.seleccionado = false;
     }
 
-    detalles.innerHTML = `
-        <h2>Detalle del Producto</h2>
-        <p><strong>ID </strong>${producto.id}</p>
-        <p><strong>Nombre </strong>${producto.nombre}</p>
-        <p><strong>Precio </strong>$${producto.precio}.-</p>
-        <p><strong>Descripcion </strong>${producto.descripcion}</p>
-        <button onclick="document.getElementById('detalle-producto').remove()">Cerrar</button>
-    `;
-
+    toggleSeleccion() {
+        this.seleccionado = !this.seleccionado;
+    }
 }
+
+let productos = [];
+let carrito = [];
+
+// Referencias al DOM
+const contenedorProductos = document.getElementById('productos');
+const listaCarrito = document.getElementById('lista-carrito');
+const totalElemnto = document.getElementById('total');
+
+function manejarSeleccion(producto) {
+    producto.toggleSeleccion();
+
+    if (producto.seleccionado) {
+        carrito.push(producto)
+    } else {
+        carrito = carrito.filter(p => p.id !== producto.id);
+    }
+
+    renderizarProductos();
+    renderizarCarrito();
+}
+
+function renderizarProductos() {
+    contenedorProductos.innerHTML = '';
+
+    productos.forEach(producto => {
+        const div = document.createElement('div');
+        div.classList.add('producto');
+        if (producto.seleccionado) div.classList.add('seleccionado');
+
+        div.innerHTML = `
+            <img src="${producto.image}" alt="${producto.nombre}" width=90 />
+            <h2>${producto.nombre}</h2>
+            <p><strong>Descripcion: </strong>${producto.descripcion}</p>
+            <P><strong>Precio: </strong> $<span>${producto.precio}</span>.-</P>
+            <button>${producto.seleccionado ? 'Quitar del Carrito' : 'Agregar al Carrito'}</button>
+        `;
+        const boton = div.querySelector('button');
+        boton.addEventListener('click', () => manejarSeleccion(producto));
+
+        contenedorProductos.appendChild(div);
+    });
+}
+
+function renderizarCarrito() {
+    listaCarrito.innerHTML = '';
+
+    carrito.forEach(p => {
+        const li = document.createElement('li');
+        li.textContent = `${p.nombre} - $${p.precio}.-`;
+        listaCarrito.appendChild(li);
+    });
+
+    const total = carrito.reduce((acc, prod) => acc + prod.precio, 0);
+    totalElemnto.textContent = total;
+}
+
+async function cargarProductos() {
+    return await fetch('productos.json')
+        .then(res => {
+            if(!res.ok) throw new Error('No se pudo cargar el archivo JSON');
+            return res.json();
+        })
+        .then(data => {
+            productos = data.map(p => new Producto(p.id, p.nombre, p.image, p.precio, p.descripcion));
+            renderizarProductos();
+        })
+        .catch(err => {
+            contenedorProductos.innerHTML = `<p style="color: red;">${err.message}</p>`;
+        })
+}
+
+cargarProductos();
